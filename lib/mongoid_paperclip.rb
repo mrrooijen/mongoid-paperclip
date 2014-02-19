b92dfeb247a61db2a0556e71225aa64d4ffc027e
Florent Morin <florent.morin@morin-innovation.com>
2014-02-19 12:19:15 +0100

    Add after_commit support
    If not, application crashes on Heroku.

--- lib/mongoid_paperclip.rb
+++ lib/mongoid_paperclip.rb
@@ -62,6 +62,26 @@ module Mongoid
     end
 
     module ClassMethods
+    
+      ##
+      # Adds after_commit
+      def after_commit(*args, &block)
+        options = args.pop if args.last.is_a? Hash
+        if options
+          case args[:on]
+          when :create
+            after_create(*args, &block)
+          when :update
+            after_update(*args, &block)
+          when :destroy
+            after_destroy(*args, &block)
+          else
+            after_save(*args, &block)
+          end
+        else
+          after_save(*args, &block)
+        end
+      end
 
       ##
       # Adds Mongoid::Paperclip's "#has_mongoid_attached_file" class method to the model
